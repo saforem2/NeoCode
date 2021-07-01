d6c8f290aa1ef5339bf81bb4607161e761821e77
Rafael Madriz <rafaelcontacto@pm.me>
2021-06-30 21:01:29 -0300

    update nvim-tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,51 +1,63 @@
-vim.g.nvim_tree_indent_markers = 1
-vim.g.nvim_tree_width = 35
+vim.g.nvim_tree_side = vim.g.neon_explorer_side or "right"
+vim.g.nvim_tree_width = 40
+vim.g.nvim_tree_ignore = {".git", "node_modules", ".cache"}
+vim.g.nvim_tree_highlight_opened_files = 1
+vim.g.nvim_tree_add_trailing = 1
 vim.g.nvim_tree_gitignore = 1
 vim.g.nvim_tree_follow = 1
 vim.g.nvim_tree_disable_netrw = 0
 vim.g.nvim_tree_hijack_netrw = 0
 vim.g.nvim_tree_auto_close = 1
-vim.g.nvim_tree_ignore = {".git", "node_modules", ".cache"}
 vim.g.nvim_tree_lsp_diagnostics = 1
-vim.g.nvim_tree_auto_ignore_ft = "startify"
 vim.g.nvim_tree_quit_on_open = 0
 vim.g.nvim_tree_tab_open = 0
-vim.g.nvim_tree_side = vim.g.neon_explorer_side or "right"
+vim.g.nvim_tree_update_cwd = 1
 
 vim.g.nvim_tree_icons = {
-    default = "",
-    symlink = "",
-    git = {unstaged = "", staged = "✓", unmerged = "", renamed = "➜", untracked = ""},
-    folder = {default = "", open = "", empty = "", empty_open = "", symlink = ""}
+    git = {unstaged = "✗", staged = "✓", unmerged = "", renamed = "➜", untracked = "", ignored = "◌"},
+    folder = {
+        arrow_open = "",
+        arrow_closed = "",
+        default = "",
+        open = "",
+        empty = "",
+        empty_open = "",
+        symlink = ""
+    }
 }
 
 local tree_cb = require "nvim-tree.config".nvim_tree_callback
+-- default mappings
 vim.g.nvim_tree_bindings = {
-    -- mappings
-    ["<CR>"] = tree_cb("edit"),
-    ["o"] = tree_cb("edit"),
-    ["<2-LeftMouse>"] = tree_cb("edit"),
-    ["<2-RightMouse>"] = tree_cb("cd"),
-    ["<S-c>"] = tree_cb("cd"),
-    ["v"] = tree_cb("vsplit"),
-    ["s"] = tree_cb("split"),
-    ["<C-t>"] = tree_cb("tabnew"),
-    ["h"] = tree_cb("close_node"),
-    ["<BS>"] = tree_cb("close_node"),
-    ["<S-CR>"] = tree_cb("close_node"),
-    ["<Tab>"] = tree_cb("preview"),
-    ["I"] = tree_cb("toggle_ignored"),
-    ["H"] = tree_cb("toggle_dotfiles"),
-    ["R"] = tree_cb("refresh"),
-    ["a"] = tree_cb("create"),
-    ["d"] = tree_cb("remove"),
-    ["r"] = tree_cb("rename"),
-    ["<C-r>"] = tree_cb("full_rename"),
-    ["x"] = tree_cb("cut"),
-    ["c"] = tree_cb("copy"),
-    ["p"] = tree_cb("paste"),
-    ["[c"] = tree_cb("prev_git_item"),
-    ["]c"] = tree_cb("next_git_item"),
-    ["-"] = tree_cb("dir_up"),
-    ["q"] = tree_cb("close")
+    {key = {"<CR>", "o", "<2-LeftMouse>"}, cb = tree_cb("edit")},
+    {key = {"<2-RightMouse>", "C"}, cb = tree_cb("cd")},
+    {key = "<C-v>", cb = tree_cb("vsplit")},
+    {key = "<C-s>", cb = tree_cb("split")},
+    {key = "<C-t>", cb = tree_cb("tabnew")},
+    {key = "<", cb = tree_cb("prev_sibling")},
+    {key = ">", cb = tree_cb("next_sibling")},
+    {key = "P", cb = tree_cb("parent_node")},
+    {key = "<BS>", cb = tree_cb("close_node")},
+    {key = "<S-CR>", cb = tree_cb("close_node")},
+    {key = "<Tab>", cb = tree_cb("preview")},
+    {key = "K", cb = tree_cb("first_sibling")},
+    {key = "J", cb = tree_cb("last_sibling")},
+    {key = "I", cb = tree_cb("toggle_ignored")},
+    {key = "H", cb = tree_cb("toggle_dotfiles")},
+    {key = "R", cb = tree_cb("refresh")},
+    {key = "a", cb = tree_cb("create")},
+    {key = "d", cb = tree_cb("remove")},
+    {key = "r", cb = tree_cb("rename")},
+    {key = "<C-r>", cb = tree_cb("full_rename")},
+    {key = "x", cb = tree_cb("cut")},
+    {key = "c", cb = tree_cb("copy")},
+    {key = "p", cb = tree_cb("paste")},
+    {key = "y", cb = tree_cb("copy_name")},
+    {key = "Y", cb = tree_cb("copy_path")},
+    {key = "gy", cb = tree_cb("copy_absolute_path")},
+    {key = "[g", cb = tree_cb("prev_git_item")},
+    {key = "]g", cb = tree_cb("next_git_item")},
+    {key = "-", cb = tree_cb("dir_up")},
+    {key = "q", cb = tree_cb("close")},
+    {key = "g?", cb = tree_cb("toggle_help")}
 }
