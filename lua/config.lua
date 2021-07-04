9455f7dfb889e06795e4357126fd86c8088dea29
Rafael <rafaelcontacto@pm.me>
2021-07-04 18:21:49 -0300

    revert to default theme

--- lua/config.lua
+++ lua/config.lua
@@ -23,7 +23,7 @@ g.neon_indent_size = 4
 --- Colors/Style
 -----------------------------------------------------------------------------//
 -- Press <space>ft to see all available themes
-g.neon_colorscheme = "darcula"
+g.neon_colorscheme = "neon"
 -- See :h themes-nvim for help
 g.themes_italic_comment = true
 g.themes_italic_keyword = true
@@ -31,9 +31,9 @@ g.themes_italic_boolean = true
 g.themes_italic_function = true
 g.themes_italic_variable = false
 -- @options: classic, arrow, slant
-g.neon_statusline = "arrow"
+g.neon_statusline = "classic"
 -- @options: gruvbox, neon, nord, wombat
-g.neon_statusline_color = "wombat"
+g.neon_statusline_color = "neon"
 
 -----------------------------------------------------------------------------//
 --- LSP
