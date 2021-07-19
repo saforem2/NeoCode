461a1003faef531f977f4b2ea128de1e50e1ea05
Rafael Madriz <rafaelcontacto@pm.me>
2021-07-19 18:14:27 -0300

    update header

--- lua/config.lua
+++ lua/config.lua
@@ -182,11 +182,11 @@ local M = {
     -- @options: set it equal to "cowsay" to get a cow and a random quote.
     -- You can also set an ascii art as header.
     startify_header_ascii = {
-        "Y88b Y88                           Y8b Y88888P ,e,            ",
-        " Y88b Y8  ,e e,   e88 88e  888 8e   Y8b Y888P   '  888 888 8e ",
-        "b Y88b Y d88 88b d888 888b 888 88b   Y8b Y8P   888 888 888 88b",
-        "8b Y88b  888   , Y888 888P 888 888    Y8b Y    888 888 888 888",
-        "88b Y88b  'YeeP'  '88 88'  888 888     Y8P     888 888 888 888",
+        "Y88b Y88                     e88'Y88                888         ",
+        " Y88b Y8  ,e e,   e88 88e   d888  'Y  e88 88e   e88 888  ,e e,  ",
+        "b Y88b Y d88 88b d888 888b C8888     d888 888b d888 888 d88 88b ",
+        "8b Y88b  888   , Y888 888P  Y888  ,d Y888 888P Y888 888 888   , ",
+        "88b Y88b  'YeeP'  '88 88'    '88,d88  '88 88'   '88 888  'YeeP' ",
     },
     -- }}}
 }
