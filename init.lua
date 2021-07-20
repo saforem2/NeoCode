f92abe55b5076d43a178cb7a517609c7c63866ac
Rafael Madriz <rafaelcontacto@pm.me>
2021-07-19 21:24:46 -0300

    avoid errors when auto-bootstrap

--- init.lua
+++ init.lua
@@ -4,8 +4,12 @@ require "core.mappings"
 require "modules.plugins"
 local async
 async = vim.loop.new_async(vim.schedule_wrap(function()
-    if vim.api.nvim_buf_get_name(0):len() == 0 then
-        vim.cmd "Startify"
+    local compiled_plugins_path =
+        vim.fn.expand "$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim/plugin/packer_compiled.lua"
+    if vim.fn.filereadable(compiled_plugins_path) > 0 then
+        if vim.api.nvim_buf_get_name(0):len() == 0 then
+            vim.cmd "Startify"
+        end
     end
     async:close()
 end))
